63f55b54fddd453367b0fdb7a17639f117cafca4
phoenixAja <phoenix@phoenixlogan.net>
2015-11-20 11:11:00 -0800

    mock netflix site

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by plogan on 11/20/15.
+ */
+var windowWidth = window.innerWidth;
+var  windowHeight = window.innerHeight;
+
+document.getElementById("topContainer").width = windowWidth;
+document.getElementById("topContainer").height = windowHeight;
+
+
+
